af598d8f1564734080f88c29490934d1a555a825
steadyforge <77584386+steadyforge@users.noreply.github.com>
2021-04-15 12:28:56 -0500

    Add files via upload
    updated autostart misc code fixes

--- autostart.sh
+++ autostart.sh
@@ -12,24 +12,25 @@ fi
 if [ -f "$VIDEO" ]; then
 omxplayer -o alsa $VAR
 fi
+if [ -f "$MUSIC" ]; then
+sudo mpg123 -Z /home/pi/RetroPie/music/*.mp3 >/dev/null 2>&1 &
+fi
 
-if [ -f "$ROTATE" ]; then
+if [ -f $ROTATE ];
+then
 sudo rm /opt/retropie/configs/all/retroarch.cfg
 sudo cp /home/pi/steadyforge/rotate/retroarch2.cfg /opt/retropie/configs/all/retroarch.cfg
-sudo rm /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo cp /home/pi/steadyforge/rotate/runcommand2.sh /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo chmod -x /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo chmod 777 /opt/retropie/supplementary/runcommand/runcommand.sh
-emulationstation --screenrotate 3 --screensize 780 1020 >/dev/null 2>&1 &
+#sudo rm /opt/retropie/supplementary/runcommand/runcommand.sh
+cp /home/pi/steadyforge/runcommand.sh /opt/retropie/supplementary/runcommand/
+#sudo chmod -x /opt/retropie/supplementary/runcommand/runcommand.sh
+#sudo chmod 777 /opt/retropie/supplementary/runcommand/runcommand.sh
+emulationstation --screenrotate 3 --screensize 470 655
 else
 sudo rm /opt/retropie/configs/all/retroarch.cfg
 sudo cp /home/pi/steadyforge/rotate/retroarch.cfg /opt/retropie/configs/all/retroarch.cfg
-sudo rm /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo cp /home/pi/steadyforge/rotate/runcommand.sh /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo chmod -x /opt/retropie/supplementary/runcommand/runcommand.sh
-sudo chmod 777 /opt/retropie/supplementary/runcommand/runcommand.sh
-emulationstation >/dev/null 2>&1 &
-fi
-if [ -f "$MUSIC" ]; then
-sleep 50; sudo mpg123 -Z /home/pi/RetroPie/music/*.mp3 >/dev/null 2>&1
+#sudo rm /opt/retropie/supplementary/runcommand/runcommand.sh
+cp /home/pi/steadyforge/rotate/runcommand.sh /opt/retropie/supplementary/runcommand/
+#sudo chmod -x /opt/retropie/supplementary/runcommand/runcommand.sh
+#sudo chmod 777 /opt/retropie/supplementary/runcommand/runcommand.sh
+emulationstation
 fi
