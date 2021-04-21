1348f5d63dabf841a80c63b846d712f41eb25dbc
steadyforge <77584386+steadyforge@users.noreply.github.com>
2021-04-20 22:36:08 -0500

    Add files via upload
    System bootup videos added.

---
+++ retropiemenu/bootupscreen-off.sh
@@ -0,0 +1,3 @@
+sudo rm /home/pi/steadyforge/boot/videolaunch.sh
+whiptail --msgbox "System Bootup Video: Disabled." 10 20 &lt;/dev/tty &gt;/dev/tty
+sleep 2
---
+++ retropiemenu/bootupscreen-on.sh
@@ -0,0 +1,3 @@
+sudo cp /home/pi/steadyforge/backup/videolaunch.sh /home/pi/steadyforge/boot/videolaunch.sh
+whiptail --msgbox "System Bootup Video: enabled." 10 20 &lt;/dev/tty &gt;/dev/tty
+sleep 2
