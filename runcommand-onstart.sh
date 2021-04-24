8dc22b5ccb42c818539272bbad68079d4ec53607
steadyforge <77584386+steadyforge@users.noreply.github.com>
2021-04-23 22:39:46 -0500

    Add files via upload
    update 0.0.3

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -12,37 +12,37 @@ fi
 if [ -f "$ROTATE" ]; then
 VAR=270
 else
-VAR=0
+VAR=270
 fi
 
 if [ -f "$VIDEOLAUNCH" ]; then
 if [ $1 = saturn ];then
-     omxplayer --orientation $VAR --blank $START/saturn.mp4
+     omxplayer --aspect-mode stretch --orientation $VAR --blank $START/saturn.mp4
 elif [ $1 = dreamcast  ];then
-    omxplayer --orientation $var --blank $START/dreamcast.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/dreamcast.mp4
 elif [ $1 = sega32x  ];then
-    omxplayer --orientation $var --blank $START/segacd.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/segacd.mp4
 elif [ $1 = snes ];then
-    omxplayer --orientation $var --blank $START/snes.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/snes.mp4
 elif [ $1 = gb ];then
-    omxplayer --orientation $var --blank $START/gb.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/gb.mp4
 elif [ $1 = gamegear ];then
-    omxplayer --orientation $var --blank $START/gamegear.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/gamegear.mp4
 elif [ $1 = gbc ];then
-    omxplayer --orientation $var --blank $START/gbc.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/gbc.mp4
 elif [ $1 = gba ];then
-    omxplayer --orientation $var --blank $START/gba.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/gba.mp4
 elif [ $1 = n64 ];then
-    omxplayer --orientation $var --blank $START/n64.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/n64.mp4
 elif [ $1 = psx ];then
-    omxplayer --orientation $var --blank $START/psx.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/psx.mp4
 elif [ $1 = segacd ];then
-    omxplayer --orientation $var --blank $START/segacd.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/segacd.mp4
 elif [ $1 = sg-1000 ];then
-    omxplayer --orientation $var --blank $START/sg-1000.mp4
+    omxplayer --aspect-mode stretch --orientation $VAR --blank $START/sg-1000.mp4
 
    else
-     omxplayer --orientation $var --blank $START/default.mp4
+     omxplayer --orientation $VAR --blank $START/default.mp4
    fi
 fi
 fi
---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Press any key to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+else
+echo "waiting for the keypress"
+fi
+done
