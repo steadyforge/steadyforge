f247d7e866c895262901375e23e092644436cb33
steadyforge <77584386+steadyforge@users.noreply.github.com>
2021-05-18 14:51:32 -0500

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -10,14 +10,9 @@ VAR=0
 fi
 
 sudo chmod -R 777 /opt/retropie/
-if [ -f "$ROTATE" ]; then
-INTRO=/home/pi/RetroPie/splashscreens/steadyforge2.mp4
-else
-INTRO=/home/pi/RetroPie/splashscreens/steadyforge.mp4
-fi
 
 if [ -f "$VIDEO" ]; then
-omxplayer --aspect-mode stretch --orientation $VAR --blank $INTRO
+omxplayer --aspect-mode stretch --orientation $VAR --blank /home/pi/RetroPie/splashscreens/steadyforge.mp4
 fi
 
 if [ -f "$MUSIC" ]; then
@@ -29,7 +24,7 @@ then
 sudo rm /opt/retropie/configs/all/retroarch.cfg
 sudo cp /home/pi/steadyforge/rotate/retroarch2.cfg /opt/retropie/configs/all/retroarch.cfg
 cp /home/pi/steadyforge/runcommand.sh /opt/retropie/supplementary/runcommand/
-emulationstation --screenrotate 3 --screensize 470 655
+emulationstation --screenrotate 3 --screensize 470 660
 else
 sudo rm /opt/retropie/configs/all/retroarch.cfg
 sudo cp /home/pi/steadyforge/rotate/retroarch.cfg /opt/retropie/configs/all/retroarch.cfg
